b7effbb65bfbb1f17d0918b97d2cb14e59fdd74e
joneskoo <joneskoo@kapsi.fi>
2010-03-30 09:39:49 +0000

    Pinger simplified and improved

--- etc/manager/pinger.sh
+++ etc/manager/pinger.sh
@@ -9,15 +9,8 @@ else
 	interface=$1
 fi
 
-#if [ $interface == "hso0" ]; then
-#    exit 1
-#fi
-
-for host in $hosts; do
-	if ping -I $interface -q -c2 -W 4 -i 0.5 $host >/dev/null 2>/dev/null
-	then
-		exit 0
-	fi
-done
+if [ "`fping -a -I $interface $hosts`" != "" ]; then
+    exit 0
+fi
 
 exit 1
